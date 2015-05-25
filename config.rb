2fe3c0bc6372f5b0ea6682a7f990b4ff329942f6
Alex Laughnan <alex.laughnan@metaltoad.com>
2015-05-25 16:45:43 -0700

    Add in compress piece to the config.rb file.

--- config.rb
+++ config.rb
@@ -7,3 +7,15 @@ javascripts_dir = "js"
 # themes can be installed in multiple locations, we don't need to worry about
 # the absolute path to the theme from the server root.
 relative_assets = true
+
+# Change this to :production when ready to deploy the CSS to the live server.
+# environment = :development
+environment = :production
+
+##
+## You probably don't need to edit anything below this.
+##
+
+# You can select your preferred output style here (can be overridden via the command line):
+# output_style = :expanded or :nested or :compact or :compressed
+output_style = (environment == :development) ? :expanded : :compressed
